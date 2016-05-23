e629b42fee2cd64b42ae177bdd2b60eae6c2009b
jhuni45 <jhunier12@gmail.com>
2016-05-23 16:53:53 -0500

    Agree

--- OAD-Plooter/types.h
+++ OAD-Plooter/types.h
@@ -1,5 +1,14 @@
 #ifndef TYPES
 #define TYPES
+#include <vector>
 
+typedef int Entero;
+typedef double Real;
+typedef float Flotante;
+typedef char cz;
+typedef string sz;
+typedef vector<Entero> lista;
+typedef vector<vector<Entero>> matriz;
+
+//averiguar vtk
 #endif // TYPES
-
